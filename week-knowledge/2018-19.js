79db68e79f31063076fb83674a921c7c2602a678
Sunny <li_qing890501@163.com>
2018-05-07 18:11:41 +0800

    转义字符

--- week-knowledge/2018-19.js
+++ week-knowledge/2018-19.js
@@ -8,3 +8,11 @@ echo 插件实现图片懒加载
     var coords = el.getBoundingClientRect();
     // 返回值是一个 DOMRect 对象，这个对象是由该元素的 getClientRects() 方法返回的一组矩形的集合, 即：是与该元素相关的CSS 边框集合 。
     // top left right bottom 都是相对于左上
+3、
+    转义字符：
+    字符	十进制	转义字符
+    "	    &#34;	&quot;
+    &	    &#38;	&amp;
+    <	    &#60;	&lt;
+    >	    &#62;	&gt;
+    不断开空格(non-breaking space)	&#160;	&nbsp;
